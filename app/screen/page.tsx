6e08b5ad19fbb8f6850a51f0548dcb66d21b4c7e
horii203 <89522294+horii203@users.noreply.github.com>
2026-04-18 23:30:16 +0900

    add upload and screen pages

---
+++ app/screen/page.tsx
@@ -0,0 +1,7 @@
+export default function ScreenPage() {
+  return (
+    <main>
+      <h1>スライドショー</h1>
+    </main>
+  );
+}
---
+++ app/upload/page.tsx
@@ -0,0 +1,7 @@
+export default function UploadPage() {
+  return (
+    <main>
+      <h1>写真をアップロード</h1>
+    </main>
+  );
+}
